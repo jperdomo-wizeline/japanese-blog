580bdc3c493bae67acbf3771a71f3973a720e278
jperdomo-wizeline <joanna.perdomo@wizeline.com>
2020-06-20 12:45:54 -0500

    uploading gatsby page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{ color: `purple` }}>
+    <Header />
+    <p>This is a test</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
 }
